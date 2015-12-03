98970b26521fe22174a36e8658f68a31fec6b519
Paul Smith <paul@freeagent.com>
2015-12-03 09:09:39 +0000

    Add serial port gateway

--- src/server.js
+++ src/server.js
@@ -1,10 +1,32 @@
 var express = require('express');
 var app = express();
+var serialport = require('serialport');
+var config = require("../config/gateway.json")["serial"]
+
 app.use(express.static('public'));
 
 // app.get('/', function (req, res) {
 //   res.send('Hello World!');
 // });
+
+
+var SerialPort  = serialport.SerialPort;
+var portName = config.port;
+var portConfig = {
+  baudRate: config.baudRate,
+  parser: serialport.parsers.readline('\n')
+};
+
+// open the serial port:
+var serialPort = new SerialPort(portName, portConfig);
+
+serialPort.on("open", function () {
+  console.log('open');
+});
+
+serialPort.on('data', function(data) {
+  console.log('data received: ' + data);
+});
 
 var server = app.listen(3000, function () {
   var host = server.address().address;
