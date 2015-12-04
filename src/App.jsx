e33d6d1266f367e115fe7694b343e0d87d25e2bd
Paul Smith <paul@freeagent.com>
2015-12-04 12:00:40 +0000

    Introduce react-router

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
+var ReactRouter = require('react-router');
+var Router = ReactRouter.Router;
+var Route = ReactRouter.Route;
 var Nodes = require('./components/Nodes.jsx');
 
-ReactDOM.render(
-  <Nodes />,
-  document.getElementById('ReactApp')
-);
+ReactDOM.render((
+  <Router>
+    <Route path="/" component={Nodes} />
+  </Router>
+), document.getElementById('sensors'))
